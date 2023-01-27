d5440c84862a938bea47517ebedda0267d88bb36
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-27 15:20:46 +0000

    chaining together fetches

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,10 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=0780a
     console.log(city.lat);
     console.log(city.lon);
 
+    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=0780a07cd4320778ef6285e7998f12ae`)
+
 })
 
-fetch("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0780a07cd4320778ef6285e7998f12ae")
 .then(response => response.json())
 .then(data => {
     console.log(data);
