63089e30e821d583b100ce6cc206e6f9963d06ad
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-30 20:47:10 +0000

    creating function for current weather

--- assets/script.js
+++ assets/script.js
@@ -15,10 +15,8 @@
 //   * When a user click on a city in the search history they are again presented with current and future conditions for that city
 
 // global variables
-let myAPI = "0780a07cd4320778ef6285e7998f12ae";
-let city = "";
-let previousSearch = "";
 
+let weatherNow = (event) => { 
 // put whole fetch inside an event listener
 //  pull out string from the button: use event delegation event.target.textContent
 
@@ -32,9 +30,9 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=0780a
     return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=0780a07cd4320778ef6285e7998f12ae`)
 
 })
-
 .then(response => response.json())
 .then(data => {
     console.log(data);
 })
 
+}
