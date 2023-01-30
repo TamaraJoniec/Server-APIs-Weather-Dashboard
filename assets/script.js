d6bb3b4392243f011b142a6396159cdbd6e718f3
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-30 23:16:32 +0000

    creating HTML inside script where forecast 5 will show up

--- assets/script.js
+++ assets/script.js
@@ -95,6 +95,28 @@ let displaySearches = () => {
 
 displaySearches();
 
+let forecast5 = (event) => {
+    let location = document.getElementById("search-input").value;
+ // fetch weather data
+ fetch("https://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=0780a07cd4320778ef6285e7998f12ae")
+ .then(response => response.json())
+ .then(citySearch => {
+     let city = citySearch[0]
+     console.log(city.lat);
+     console.log(city.lon);
+
+     return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=0780a07cd4320778ef6285e7998f12ae`)
+
+ })
+ .then(response => response.json())
+ .then(data => {
+     console.log(data);
+     // adding to local storage
+     addedCity(city);
+     // formula to get icon
+     let displayIcon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+ })
+};
 //Event listener for search button
 document.getElementById("search-button").addEventListener("click", (event) => {
     event.preventDefault();
