0b64ee55c7381af3c155d32cc2304025609844a5
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-30 19:34:16 +0000

    connected script to html, created new variables

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 // put whole fetch inside an event listener
 //  pull out string from the button: use event delegation event.target.textContent
+let myAPI = "0780a07cd4320778ef6285e7998f12ae";
+let city = "";
+let previousSearch = "";
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=0780a07cd4320778ef6285e7998f12ae")
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=0780a07cd4320778ef6285e7998f12ae")
 .then(response => response.json())
 .then(citySearch => {
     let city = citySearch[0]
     console.log(city.lat);
     console.log(city.lon);
 
-    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=0780a07cd4320778ef6285e7998f12ae`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=0780a07cd4320778ef6285e7998f12ae`)
 
 })
 
@@ -16,3 +19,5 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=0780a0
 .then(data => {
     console.log(data);
 })
+
+
