7ff6a8c12723ce4a73a8c5de05f77f4584c7fd47
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-24 22:42:55 +0000

    refactoring code in js to improve performance

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,43 @@
-
-let lat = 0;
-let lon = 0;
 let dt = 0;
-let temp;
-let name;
-let displayIcon;
 let cityFind = 0;
-let imageSource;
 let newList;
 let cityTitle = document.getElementById("city-title");
 let icon = document.getElementById("weather-icon");
 let temperature = document.getElementById("temperature");
 let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
-let myCitySearch = document.querySelector("#search-input").value;
+const listCities = document.querySelector(".list-cities");
+const historyList = document.getElementById("history-list");
+const searchInput = document.querySelector("#search-input");
+const searchResults = document.querySelector(".search-results");
+
 
 function locationURL() {
     let initialSearch = document.querySelector("#search-input").value;
     console.log(initialSearch);
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${initialSearch.toLowerCase()}&limit=5&appid=bd4f86e586f7c181c1e585358d3c507c`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (citySearch) {
+        .then(response => response.json())
+        .then(citySearch => {
             let city = citySearch[0];
             console.log(citySearch);
             weatherInfo(city);
             addedCity(city);
             weatherNow(city);
         });
-};
-// hero banner with today's weather displayed.
+}
+
 function weatherNow(city) {
-    // API endpoint for current weather data
-    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const apiKey = "bd4f86e586f7c181c1e585358d3c507c";
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&units=metric&appid=${apiKey}`;
 
     fetch(currentWeatherURL)
-        .then((response) => response.json())
-        .then((data) => {
-            // Code to display today's weather
+        .then(response => response.json())
+        .then(data => {
             const todayWeather = document.querySelector("#today-weather");
             const hero = document.getElementById("hero");
             hero.classList.add("hero-banner--show");
-            // Clearing existing content
             todayWeather.innerHTML = "";
 
-            // Creating and append elements
             const todayCityName = document.createElement("h2");
             todayCityName.textContent = data.name;
             todayWeather.appendChild(todayCityName);
@@ -74,14 +66,12 @@ function weatherNow(city) {
             )} km/h`;
             todayWeatherInfo.appendChild(todayWind);
 
-            // get the HTML elements of the hero banner
-            const banner = document.getElementById("hero-banner").style.display = "block";
+            const banner = document.getElementById("hero-banner");
             const bannerIcon = banner.querySelector(".icon");
             const bannerTemp = banner.querySelector(".banner-temp");
             const bannerHumidity = banner.querySelector(".banner-humidity");
             const bannerWind = banner.querySelector(".banner-wind");
 
-            // update the HTML of the hero banner with today's weather data
             bannerIcon.setAttribute(
                 "src",
                 `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
@@ -95,10 +85,8 @@ function weatherNow(city) {
                 data.wind.speed
             )} km/h`;
 
-            // make the hero banner visible
             banner.style.display = "block";
 
-            // Setting the font size and weight of today's weather to be bigger and bolder
             todayCityName.style.fontSize = "5rem";
             todayCityName.style.fontWeight = "bold";
             todayDate.style.fontSize = "2rem";
@@ -112,16 +100,16 @@ function weatherNow(city) {
 }
 
 // Five day weather forecast
+
 function weatherInfo(city) {
     const forecastContainer = document.getElementById("forecast-container");
     forecastContainer.innerHTML = ""; // clear old weather cards
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=bd4f86e586f7c181c1e585358d3c507c&units=imperial&cnt=40`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
+        .then(response => response.json())
+        .then(data => {
             const currentDate = new Date();
+            const weatherCards = [];
 
             for (let i = 0; i < 5; i++) {
                 const forecastDate = new Date(currentDate.getTime() + i * 24 * 60 * 60 * 1000);
@@ -149,74 +137,65 @@ function weatherInfo(city) {
                 weatherCard.appendChild(forecastWind);
                 weatherCard.appendChild(forecastHumidity);
 
-                forecastContainer.appendChild(weatherCard);
+                weatherCards.push(weatherCard);
+            }
 
-            };
-            const heroBanner = document.getElementById("hero-banner");
-            heroBanner.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${city.name}")`;
-            heroBanner.style.display = "block";
-
+            forecastContainer.append(...weatherCards);
         });
-};
+}
 
 function init() {
     const form = document.querySelector('#search-form');
     form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = document.querySelector('#search-input');
-      const query = input.value;
-      weatherInfo(query);
-      input.value = '';
+        event.preventDefault();
+        const input = document.querySelector('#search-input');
+        const query = input.value;
+        weatherInfo(query);
+        input.value = '';
     });
-  };
-  
+}
+
 function addedCity(city) {
-    newList = findData();
-    cityFind = document.createElement("div");
+    const newList = findData();
+    const cityFind = document.createElement("div");
     cityFind.setAttribute("id", city.name);
     cityFind.textContent = city.name;
     cityFind.classList.add("h5");
 
-    if (newList.includes(city.name) === false) {
-        document.querySelector(".list-cities").appendChild(cityFind);
+    if (!newList.includes(city.name)) {
+        listCities.appendChild(cityFind);
     }
 
     document.querySelector(".subheading").setAttribute("style", "display:inline");
-    city
 
     function findData() {
-        let currentList = localStorage.getItem("city");
-        if (currentList !== null) {
-            newList = JSON.parse(currentList);
-        } else {
-            newList = [];
-        }
+        const currentList = localStorage.getItem("city");
+        const newList = currentList !== null ? JSON.parse(currentList) : [];
         return newList;
-    }
+}
 };
-
 // adding city to local storage
 function cityLocalStorage(city) {
-    let addedList = findData();
-
+    const addedList = findData();
+  
     if (!addedList.includes(city)) {
-        addedList.push(city);
+      addedList.push(city);
     }
+  
     localStorage.setItem("city", JSON.stringify(addedList));
-};
+  }
 
 // Function for displaying cities
 function displayData() {
-    let searchRecord = findData();
-    let listCities = document.querySelector(".list-cities");
+    const searchRecord = findData();
+    const listCities = document.querySelector(".list-cities");
+    let citiesHtml = "";
 
     for (let i = 0; i < searchRecord.length; i++) {
-        let myCitySearch = document.createElement("h4");
-        myCitySearch.setAttribute("id", searchRecord[i].name);
-        myCitySearch.textContent = searchRecord[i].name;
-        myCitySearch.classList.add("h4");
-        listCities.append(myCitySearch);
+        citiesHtml += `<h4 id="${searchRecord[i].name}" class="h4">${searchRecord[i].name}</h4>`;
     }
+
+    listCities.innerHTML = citiesHtml;
 
     const historyList = document.getElementById("history-list");
     const historyItem = document.createElement("button");
@@ -227,17 +206,38 @@ function displayData() {
     });
     historyList.appendChild(historyItem);
 
-
 };
+
+// debounce the search input
+let timeoutId;
+searchInput.addEventListener("input", function () {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(locationURL, 500);
+});
+
 
 // Add the search term to the search history
 function addToHistory(searchTerm) {
     // Get the existing search history
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-  
     // Add the new search term to the search history
     searchHistory.push(searchTerm);
 
-}  
+}
 //Event listeners for search button
 document.getElementById("search-button").addEventListener('click', locationURL);
+
+// attach a click event listener to the parent element
+searchResults.addEventListener("click", function (event) {
+    // check if the click event was triggered by a search result
+    if (event.target.classList.contains("search-result")) {
+        // get the city object from the data attribute of the search result
+        const city = JSON.parse(event.target.dataset.city);
+        // update the weather information for the selected city
+        weatherInfo(city);
+        addedCity(city);
+        weatherNow(city);
+        // clear the search results
+        searchResults.innerHTML = "";
+    }
+});
